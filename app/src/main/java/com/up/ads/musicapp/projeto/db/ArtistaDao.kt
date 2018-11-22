d07fc996c2a5661959494cc61579c2118964ef81
Mateus Shimomura <mateus_shimomura@hotmail.com>
2018-11-22 20:28:54 -0200

    ArtistaDao

--- app/src/main/java/com/up/ads/musicapp/projeto/db/ArtistaDao.kt
+++ app/src/main/java/com/up/ads/musicapp/projeto/db/ArtistaDao.kt
@@ -1,9 +1,19 @@
 package com.up.ads.musicapp.projeto.db
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
 
 @Dao
 interface ArtistaDao {
 
-    fun insert()
+    @Insert
+    fun insert(artista: Artista)
+
+    @Query("DELETE FROM artista where id = :id")
+    fun delete(id: Long)
+
+    @Query("SELECT * FROM artista")
+    fun get(): LiveData<List<Artista>>
 }
